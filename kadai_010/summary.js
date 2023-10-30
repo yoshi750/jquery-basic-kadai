5217cca08806bec19c9f7ad4e9b76754d1b0dae5
yoshi750 <146502759+yoshi750@users.noreply.github.com>
2023-10-30 23:09:28 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,16 @@
+$("#change-color").on('click',function() {
+    $("#target").css('color','red');
+});
+
+$("#change-text").on('click',function() {
+    $("#target").text("おはようございます！");
+});
+
+$("#fade-out").on('click',function() {
+    $("#target").fadeOut();
+});
+
+$("#fade-in").on('click',function() {
+    $("#target").fadeIn();
+});
+
